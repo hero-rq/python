15690733042b506e103ec93c8abb747b3b8cd5cf
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-08-26 12:11:23 +0900

    Create format.py

---
+++ practice/format.py
@@ -0,0 +1,13 @@
+import time 
+a = 1
+b = 'panda'
+print(a, b, end='\r')
+time.sleep(1)
+print(2, b, end='\r')
+time.sleep(1)
+print(3, b, end='\r')
+time.sleep(1)
+print(4, b, end='\r')
+time.sleep(1)
+print(5, b, end='\r')
+time.sleep(1)
