dcac4f96185032a4f1b72ebd94b0ae2ddc7b322c
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-04 19:27:07 +0900

    Create mysterious.py

---
+++ for_hacking/mysterious.py
@@ -0,0 +1,19 @@
+import webbrowser
+import time 
+
+list1 = [
+  "https://blog.naver.com/PostView.naver?blogId=jarid2292&logNo=222915897246",
+  "https://jarid2292.tistory.com/1888",
+]
+url = "https://jarid2292.tistory.com/1888"
+
+chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
+
+webbrowser.get('chrome').open("https://www.google.com")
+webbrowser.get('chrome').open(url)
+#webbrowser.open("https://jarid2292.tistory.com/1888")
+
+#for url in list1:
+# webbrowser.open(url)
+# time.sleep(2)
