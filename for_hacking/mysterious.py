44bed63cb194f46fc7d8c5628c62893b590cfc87
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-04 19:45:01 +0900

    Update mysterious.py

--- for_hacking/mysterious.py
+++ for_hacking/mysterious.py
@@ -1,33 +1,20 @@
 import webbrowser
-
 import time
 
-
 list1 = [
-
-"https://blog.naver.com/PostView.naver?blogId=jarid2292",
-
-"https://jarid2292.tistory.com/1888",
-
+  "https://blog.naver.com/PostView.naver?blogId=jarid2292",
+  "https://jarid2292.tistory.com/1888",
 ]
 
 url = "https://jarid2292.tistory.com/1888"
-
-
 chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-
 webbrowser.register('chrome', None, webbrowser.BackgroundBrowser(chrome_path))
 
-
 webbrowser.get('chrome').open("https://www.google.com")
-
 webbrowser.get('chrome').open(url)
 
 #webbrowser.open("https://jarid2292.tistory.com/1888")
 
-
 #for url in list1:
-
 # webbrowser.open(url)
-
 # time.sleep(2)
