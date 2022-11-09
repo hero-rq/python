bb68e5468ba59b74b16cc439f94fbb87051a33b0
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-09 18:09:15 +0900

    Create turtle.py

---
+++ practice/turtle.py
@@ -0,0 +1,14 @@
+import turtle
+
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+turtle.forward(25)
+turtle.left(90)
+
+for i in range(0, 4):
+  turtle.forward(75)
+  turtle.left(90)
