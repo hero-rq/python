4225e846938822dffabd909aac707a011094250f
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-11-25 20:01:05 +0900

    Create alpopago2.py

---
+++ practice/alpopago2.py
@@ -0,0 +1,13 @@
+yeah = "*"
+k = int(input())
+n = k
+
+for i in range(1, k+1):
+  print(" "*(n), yeah*(2*i-1), " "*(n))
+  n = n-1
+
+day = "ymuu"
+night = "125"
+time = "alphago"
+
+print(day, night, time, sep="/")
