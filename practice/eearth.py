48fc6a7627ba293813a5750a8ca3ad352259eb43
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-12-14 17:12:42 +0900

    Create eearth.py

---
+++ practice/eearth.py
@@ -0,0 +1,23 @@
+#수성, 금성, 지구, 화성, 목성, 토성, 천왕성, 해왕성 
+#Mer, Ven, Ear, Mar, Jup, Sat,  Ura,  Nep
+#한글 이름 넣으면 그거에 맞는 영어이름 
+k = input()
+
+if k == '수성':
+  print("Mer")
+elif k == '금성':
+  print("Ven")
+elif k == '지구':
+  print("Ear")
+elif k == '화성':
+  print("Mar")
+elif k == '목성':
+  print('Jup')
+elif k == '토성':
+  print('Sat')
+elif k == '천왕성':
+  print('Ura')
+elif k == '해왕성':
+  print('Nep')
+else: 
+  print('입력이 잘못되었습니다')
