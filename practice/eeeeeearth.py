0ed3cae37817749cc91b0d843142d4e1526f0e27
slow thinker <80503808+hero-rq@users.noreply.github.com>
2022-12-14 17:17:14 +0900

    Create eeeeeearth.py

---
+++ practice/eeeeeearth.py
@@ -0,0 +1,15 @@
+#수성, 금성, 지구, 화성, 목성, 토성, 천왕성, 해왕성 
+#Mer, Ven, Ear, Mar, Jup, Sat,  Ura,  Nep
+#한글 이름 넣으면 그거에 맞는 영어이름 
+solar = {
+  '수성': 'Mer', 
+  '금성': 'Ven', 
+  '지구':'Ear',
+  '화성': 'Mar',
+  '목성': 'Jup', 
+  '토성': 'Sat',  
+  '천왕성': 'Ura',  
+  '해왕성': 'Nep'
+}
+p = input()
+print(solar[p])
