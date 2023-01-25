42b7f1fec6a9079c7023a44710b2a567296b173c
slow thinker <80503808+hero-rq@users.noreply.github.com>
2023-01-25 16:15:30 +0900

    Create 20250921.py

---
+++ job_scrapper/20250921.py
@@ -0,0 +1,16 @@
+#20250921출력하기 (아래 조건 충족 o)
+#코드 내에 숫자가 없어야함 
+#시간, 날짜 함수, 파일 이름이나 경로 사용 x 
+#에러 번호 출력 x 
+#input 사용 x 
+
+s = 'bbaaaaaaaaacccccd'
+
+print(s.count('b'), end='') #2
+print(s.count('s'), end='') #0
+print(s.count('b'), end='') #2
+print(s.count('c'), end='') #5
+print(s.count('k'), end='') #0
+print(s.count('a'), end='') #9
+print(s.count('b'), end='') #2
+print(s.count('d'), end='') #1
